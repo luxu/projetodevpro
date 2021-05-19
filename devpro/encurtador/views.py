e0a8c3cbac4d3299ccd740d387a722940c819c91
Luciano Martins <zicadopv@gmail.com>
2021-05-19 12:22:49 -0300

    pagina reduce.html e os assets

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -6,5 +6,14 @@ from devpro.encurtador.models import UrlRedirect
 
 def redirecionar(request, slug):
     urlRedirect = UrlRedirect.objects.get(slug=slug)
-    # return HttpResponse(f'Olá Django: {slug}')
     return redirect(urlRedirect.destino)
+
+
+def relatorios(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = requisicao.build_absolute_uri(f'/{slug}')
+    contexto = {
+	    'reduce': url_redirect,
+	    'url_reduzida': url_reduzida
+    }
+    return render(requisicao, 'encurtador/relatorio.html', contexto)
